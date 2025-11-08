1a075b0eb85c9d83403b98b45275e6264bf3410a
projectpixel16 <jasonflor.cenpri@gmail.com>
2025-11-08 16:51:10 +0800

    jason ui

---
+++ resources/js/components/card.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="items-center justify-center py-8">
+	    <div class="bg-white rounded-2xl shadow-lg max-w-6xl mx-auto">
+            <div class="px-12 py-10">
+                <slot/>
+            </div>
+        </div>
+    </section>
+</template>
